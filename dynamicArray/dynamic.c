a4ec6f95ec0efa7ac7879871055ce5f8d6aa1ea2
felipe <feliog1234@gmail.com>
2025-05-28 19:46:08 -0600

    Terminado dynamic arr

--- dynamicArray/dynamic.c
+++ dynamicArray/dynamic.c
@@ -2,72 +2,125 @@
 #include <string.h>
 #include <stdlib.h>
 
-int get(int i,int arr[]){
+typedef struct{
+int size;
+int *arr;
+int capacity;
 
-  return arr[i];
+int numAllocations;
+
+}Vector;
+
+Vector createVector(int capa){
+
+  Vector v;
+
+  v.size=0;
+
+  v.arr=malloc(capa * sizeof(int));
+
+  v.capacity = capa;
   
-}
 
-void set(int *arr,int value,int index,int **p){
-  arr[index]=value;
-    
-  if (&arr[index]>*p){
+  return v;
 
-    *p=&arr[index];
-  } 
 
 }
 
-void pushBack(int *arr,int capacity,int value,int **p){
+void pushBack(Vector *v,int value){
  
-  int currentPos = *p - arr;
+  
+  
+  if (v->size + 1<= v->capacity){
 
-  if (currentPos>=capacity){
-    printf("No se pueden agregar mas elementos luego de la posicion %d",currentPos-1);
+    v->arr[v->size]=value;
 
-    return;
+    v->size++;
+    
+
+  }else{
+    v->capacity = v->capacity *2;
+    int *temp=realloc(v->arr,v->capacity * sizeof(int));
+    if (temp){
+      v->arr = temp;
+      v->arr[v->size]=value;
+      v->size++;
+       
+    }else{
+
+      printf("No se logro realocar memoria en el heap\n");
+      exit(1);
+   }
+
+
+    
   }
 
-    *p = *p +1;
 
-    **p = value;
+
+    printf("La cantidad de elementos agregados por el momento es de: %d y este arr tiene una capacidad de %d\n",v->size,v->capacity);   
+} 
+
+
+void MostrarArr(Vector *v){
+
+
+  for (int i=0;i<v->size;i++){
+
+    printf("%d ",v->arr[i]);
+
+
+
+  }
+
 }
 
+void MostrarMenu(){
 
-int getElements(int *arr,int capa){
-  int nums[capa];
-  int j=0; 
-  for (int i=0;i<capa;i++){
-    
-    if (arr[i] !=0){
+  printf("1.Agregar elemento al vector\n");
 
-      nums[j]=arr[i];
+  printf("2.Ver todos los elementos del vector\n");
 
-      j++;
+}
+
+int main(){
+  
+  int capacidad = 0;
+  int t=1;
+  printf("Digite la capacidad del vector:");
+
+  scanf("%d",&capacidad);
+  
+  
+  Vector v = createVector(capacidad);
+
+  while (t){
+    int res=0;
+    MostrarMenu();
+    scanf("%d",&res);
+
+    if (res==1){
+      int value=0;
+
+      printf("Digite el elemento a agregar:");
+
+      scanf("%d",&value);
+
+      pushBack(&v,value);
+
+    }else if(res==2){
+
+      MostrarArr(&v);
+
+    }else{
+
+     
+      free(v.arr);
       
 
     }
-    
-      
+
   }
-  return nums;
-
-}
-
-
-int main(){
-
-  int capacity=10;
-
-  int arr[capacity];
-
-  int *p=arr -1;
-
-
   
-
-
-   
-
   return 0;
 }
